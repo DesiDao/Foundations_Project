a20cecb8304d93e919b98f25bb2fecb2be00f68a
DesiDao <55844636+DesiDao@users.noreply.github.com>
2025-03-12 18:41:34 -0400

    Update BTree.py
    BTreeNode: No changes
    
    BTree:
    - insert (&  private implementation)
    - search (&  private implementation)

--- BTree.py
+++ BTree.py
@@ -3,3 +3,41 @@ class BTreeNode:
         self.key = key
         self.left = left
         self.right = right
+
+class BTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = BTreeNode(key)
+        else:
+            self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = BTreeNode(key)
+            else:
+                self._insert_recursive(node.left, key)
+        else:
+            if node.right is None:
+                node.right = BTreeNode(key)
+            else:
+                self._insert_recursive(node.right, key)
+
+    def search(self, key):
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, node, key):
+        if node is None or node.key == key:
+            return node
+        elif key < node.key:
+            return self._search_recursive(node.left, key)
+        else:
+            return self._search_recursive(node.right, key)
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
