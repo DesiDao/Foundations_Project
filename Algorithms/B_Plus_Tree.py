640dd0b324829427b77ae04dfd0bafb51b52c502
DesiDao <55844636+DesiDao@users.noreply.github.com>
2025-03-13 06:19:52 -0400

    Update B_Plus_Tree.py
    added search and insert implementations

--- Algorithms/B_Plus_Tree.py
+++ Algorithms/B_Plus_Tree.py
@@ -1,1 +1,32 @@
+import bisect
 
+class BPlusTreeNode:
+    def __init__(self, leaf=False):
+        self.leaf = leaf
+        self.keys = []
+        self.children = []
+        self.next = None  # Used for leaf node traversal
+
+class BPlusTree:
+    def __init__(self, order=3):
+        self.root = BPlusTreeNode(leaf=True)
+        self.order = order
+
+    def search(self, key):
+        node = self.root
+        while not node.leaf:
+            i = bisect.bisect_left(node.keys, key)
+            node = node.children[i]
+        
+        if key in node.keys:
+            return True
+        return False
+
+    def insert(self, key):
+        root = self.root
+        split_key, new_child = self._insert_recursive(root, key)
+        if new_child:
+            new_root = BPlusTreeNode()
+            new_root.keys = [split_key]
+            new_root.children = [root, new_child]
+            self.root = new_root
