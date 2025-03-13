49b706501619ca0550dbc46a533f6e73ed020459
DesiDao <55844636+DesiDao@users.noreply.github.com>
2025-03-13 06:20:36 -0400

    Update B_Plus_Tree.py
    added additional implementations

--- Algorithms/B_Plus_Tree.py
+++ Algorithms/B_Plus_Tree.py
@@ -30,3 +30,52 @@ class BPlusTree:
             new_root.keys = [split_key]
             new_root.children = [root, new_child]
             self.root = new_root
+
+    
+    def _insert_recursive(self, node, key):
+        if node.leaf:
+            bisect.insort(node.keys, key)
+            if len(node.keys) >= self.order:
+                return self._split_leaf(node)
+            return None, None
+
+        i = bisect.bisect_left(node.keys, key)
+        split_key, new_child = self._insert_recursive(node.children[i], key)
+        if new_child:
+            node.keys.insert(i, split_key)
+            node.children.insert(i + 1, new_child)
+            if len(node.keys) >= self.order:
+                return self._split_internal(node)
+        return None, None
+
+    def _split_leaf(self, node):
+        mid = len(node.keys) // 2
+        new_node = BPlusTreeNode(leaf=True)
+        new_node.keys = node.keys[mid:]
+        node.keys = node.keys[:mid]
+        
+        new_node.next = node.next
+        node.next = new_node
+        return new_node.keys[0], new_node
+
+    def _split_internal(self, node):
+        mid = len(node.keys) // 2
+        new_node = BPlusTreeNode()
+        new_node.keys = node.keys[mid + 1:]
+        new_node.children = node.children[mid + 1:]
+        node.keys = node.keys[:mid]
+        node.children = node.children[:mid + 1]
+        return node.keys[mid], new_node
+
+    def range_query(self, low, high):
+        node = self.root
+        while not node.leaf:
+            node = node.children[0]
+        
+        result = []
+        while node:
+            for key in node.keys:
+                if low <= key <= high:
+                    result.append(key)
+            node = node.next
+        return result
